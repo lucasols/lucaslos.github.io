eb07f238f40cad16dcab722c381f5ed1e0b595bd
lucaslos <lucas@sugestly.com>
2019-06-25 13:26:12 -0300

    Improve images transition

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 import css from '@emotion/css';
 import { fillContainer } from 'style/modifiers';
 import useInterval from 'utils/hooks/useInterval';
+import { colorPrimary } from 'style/theme';
+
+let id = 0;
 
 type Props = {
   imgs: string[];
@@ -11,13 +14,15 @@ type Props = {
 
 const Container = styled.div`
   overflow: hidden;
+  background: ${colorPrimary};
 `;
 
 const Img = styled.div`
   ${fillContainer}
   cursor: zoom-in;
-  transition: 240ms ease-out;
+  transition: transform 240ms ease-out, opacity 0.8s;
   background-size: cover;
+  background-position: center;
 
   ${Container}:hover & {
     transform: scale(1.05);
@@ -26,11 +31,11 @@ const Img = styled.div`
 
 const SlideShow = ({ imgs, onClick }: Props) => {
   const [activeImg, setActiveImg] = useState(0);
-  const [startTime, setStartTime] = useState(Math.random() * 5000);
+  const [startTime, setStartTime] = useState(id++ * 3000);
 
   useInterval(() => {
     if (imgs.length > 1) {
-      setStartTime(0);
+      if (startTime !== 0) setStartTime(0);
       setActiveImg(activeImg < imgs.length - 1 ? activeImg + 1 : 0);
     }
   }, 5000 + startTime);
